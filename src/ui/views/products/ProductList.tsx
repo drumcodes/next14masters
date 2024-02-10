32e699f06ef83ceeb9164b5e16461383f73a270a
drumcodes <adam.jedrasik@gmail.com>
2024-02-10 01:51:02 +0100

    change data-testid location

--- src/ui/views/products/ProductList.tsx
+++ src/ui/views/products/ProductList.tsx
@@ -7,7 +7,10 @@ export const ProductList = ({
 	products: ProductItem[];
 }) => {
 	return (
-		<ul className="grid grid-cols-1 gap-8 sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+		<ul
+			data-testid="products-list"
+			className="grid grid-cols-1 gap-8 sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+		>
 			{products.map((product) => {
 				return <ProductListItem key={product.id} product={product} />;
 			})}
