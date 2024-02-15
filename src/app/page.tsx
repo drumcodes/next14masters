969770f866ad0201f1d4b095e0b8fb940d36516a
drumcodes <adam.jedrasik@gmail.com>
2024-02-15 19:59:53 +0100

    new new test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
 	return (
 		<h1 className="mx-auto mb-5 max-w-md text-center text-4xl font-bold text-slate-600">
-			Hello new world!
+			Hello new new world!
 		</h1>
 	);
 }
