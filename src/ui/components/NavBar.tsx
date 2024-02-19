50b5bd0fd54e01317561d15448b0125dfa617b85
drumcodes <adam.jedrasik@gmail.com>
2024-02-19 13:06:50 +0100

    add exact

--- src/ui/components/NavBar.tsx
+++ src/ui/components/NavBar.tsx
@@ -3,20 +3,24 @@ import { ActiveLink } from "./ActiveLink";
 
 export const NavBar = () => {
 	return (
-		<nav className="mb-10">
-			<ul className="mt-2 flex justify-center space-x-4">
-				<li>
-					<ActiveLink href={"/"}>Home </ActiveLink>
-				</li>
-				<li>
-					<ActiveLink href={"/products" as Route}>All</ActiveLink>
-				</li>
-				<li>
-					<ActiveLink href={"/static-products"}>
-						Static Products
-					</ActiveLink>
-				</li>
-			</ul>
-		</nav>
+		<header>
+			<nav className="mb-10">
+				<ul className="mt-2 flex justify-center space-x-4">
+					<li>
+						<ActiveLink href={"/"} exact>
+							Home
+						</ActiveLink>
+					</li>
+					<li>
+						<ActiveLink href={"/products" as Route}>All</ActiveLink>
+					</li>
+					<li>
+						<ActiveLink href={"/static-products"}>
+							Static Products
+						</ActiveLink>
+					</li>
+				</ul>
+			</nav>
+		</header>
 	);
 };
