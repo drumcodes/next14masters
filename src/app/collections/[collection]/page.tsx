692329ad29809620b12114697e6c598ff6107fdd
drumcodes <adam.jedrasik@gmail.com>
2024-02-28 13:44:30 +0100

    remove incorrect static params generation

--- src/app/collections/[collection]/page.tsx
+++ src/app/collections/[collection]/page.tsx
@@ -3,18 +3,6 @@ import { executeGraphql } from "@/api/utils";
 import { ProductsGetByCollectionSlugDocument } from "@/gql/graphql";
 import { ProductList } from "@/ui/components/page/products/ProductList";
 
-export const generateStaticParams = async ({
-	params,
-}: {
-	params: { category: string; pageNumber: string };
-}) => {
-	if (params.category === "t-shirts") {
-		return [{ pageNumber: "1" }, { pageNumber: "2" }];
-	} else {
-		return [{ pageNumber: "1" }];
-	}
-};
-
 export default async function CollectionPage({
 	params,
 }: {
